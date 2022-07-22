3d64a94613f7c766d9e3cb5a8984d402dcd78a14
Cierra-Runis <2864283875@qq.com>
2022-07-22 17:22:13 +0800

    .

--- FuckQQ.js
+++ FuckQQ.js
@@ -16,28 +16,18 @@
 
 (function () {
     'use strict';
-    var clearFlag = 0;
-    var count = 3;                                              // 设置3秒后⾃动消失
-    var showModal = function () {
-        $("#my-modal-alert").toggle();                          // 显⽰模态框
-        $("#my-modal-alert").draggable({                        // 设置模态框可拖动（需要引⼊jquery-ui.min.js）
-            handle: ".modal-header"
-        });
-        clearFlag = self.setInterval("autoClose()", 1000);      // 每过⼀秒调⽤⼀次autoClose⽅法
-    }
-    var autoClose = function () {
-        if (count > 0) {
-            $("#num").html(count + "秒后窗⼝将⾃动关闭");
-            count--;
-        } else if (count <= 0) {
-            window.clearInterval(clearFlag);
-            $("#num").html("");
-            $("#my-modal-alert").fadeOut("slow");
-            count = 3;
-            $("#my-modal-alert").toggle();
+    function sleep(numberMillis) {
+        var now = new Date();
+        var exitTime = now.getTime() + numberMillis;
+        while (true) {
+            now = new Date();
+            if (now.getTime() > exitTime)
+                return;
         }
     }
 
     window.location.href = document.getElementById('url').textContent;
-    showModal();
+    window.alert("已跳转");
+    sleep(3000);
+    window.close();
 })();
