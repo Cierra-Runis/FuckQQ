c02fcc2326ba921283bae0d34f97a728d6e57a77
Cierra-Runis <2864283875@qq.com>
2022-07-22 17:09:56 +0800

    0.2功能尝试

--- FuckQQ.js
+++ FuckQQ.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            FuckQQ
 // @namespace       https://github.com/Cierra-Runis/FuckQQ
-// @version         0.1
+// @version         0.2
 // @description     拦你妈呢死妈 QQ 管家
 // @updateURL       https://raw.githubusercontent.com/Cierra-Runis/FuckQQ/master/FuckQQ.js
 // @downloadURL     https://raw.githubusercontent.com/Cierra-Runis/FuckQQ/master/FuckQQ.js
@@ -16,5 +16,28 @@
 
 (function () {
     'use strict';
-    window.location.href = document.getElementById('url').textContent
+    var clearFlag = 0;
+    var count = 3;                                              // 设置3秒后⾃动消失
+    var showModal = function () {
+        $("#my-modal-alert").toggle();                          // 显⽰模态框
+        $("#my-modal-alert").draggable({                        // 设置模态框可拖动（需要引⼊jquery-ui.min.js）
+            handle: ".modal-header"
+        });
+        clearFlag = self.setInterval("autoClose()", 1000);      // 每过⼀秒调⽤⼀次autoClose⽅法
+    }
+    var autoClose = function () {
+        if (count > 0) {
+            $("#num").html(count + "秒后窗⼝将⾃动关闭");
+            count--;
+        } else if (count <= 0) {
+            window.clearInterval(clearFlag);
+            $("#num").html("");
+            $("#my-modal-alert").fadeOut("slow");
+            count = 3;
+            $("#my-modal-alert").toggle();
+        }
+    }
+
+    window.location.href = document.getElementById('url').textContent;
+    showModal();
 })();
