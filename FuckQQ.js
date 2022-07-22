912a0a04e29cd272c9eb7b5488fedc28c01cf38d
Cierra-Runis <2864283875@qq.com>
2022-07-22 16:06:16 +0800

    .

--- FuckQQ.js
+++ FuckQQ.js
@@ -1,12 +1,15 @@
 // ==UserScript==
-// @name         FuckQQ
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  拦你妈呢死妈 QQ 管家
-// @author       https://github.com/Cierra-Runis
-// @match        https://c.pc.qq.com/*
-// @icon         https://github.com/Cierra-Runis/FuckQQ/blob/master/favicon.png?raw=truedomain=qq.com
-// @grant        none
+// @name            FuckQQ
+// @namespace       http://tampermonkey.net/
+// @version         0.1
+// @description     拦你妈呢死妈 QQ 管家
+// @updateURL       https://cdn.jsdelivr.net/gh/Cierra-Runis/FuckQQ/FuckQQ.js
+// @downloadURL     https://cdn.jsdelivr.net/gh/Cierra-Runis/FuckQQ/FuckQQ.js
+// @licence         MIT
+// @author          https://github.com/Cierra-Runis
+// @match           https://c.pc.qq.com/*
+// @icon            https://github.com/Cierra-Runis/FuckQQ/blob/master/favicon.png?raw=truedomain=qq.com
+// @grant           none
 // ==/UserScript==
 
 (function () {
